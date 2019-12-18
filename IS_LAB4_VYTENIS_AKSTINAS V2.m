20b93e45321e2919f7d397a0a9b68c9a43653dc4
VytenisAkstinas <49688833+VytenisAkstinas@users.noreply.github.com>
2019-12-18 19:03:41 +0200

    Sutvarkyta.
    Nustatyta, jog bėda buvo negražiam rašte. Nepadėjo nei bandymas gražiai parašyti, nei raidžių paryškinimas, nei užtušavimas. Problema išspręsta apmokius Matlabą su mano rašto pavyzdžiais, kitaip tariant, nepavyko lengvai išsisukti. Apmokyta  naudojant ocrTrainer. Mokymui naudoti skaičiai iš 'vienetai', 'dvejetai', 'trejetai'. "myLang.zip" yra apmokymo rezultatas, t. y., išmokta kalba.

---
+++ IS_LAB4_VYTENIS_AKSTINAS V2.m
@@ -0,0 +1,6 @@
+input = imread('t.jpg');
+atsakymas = ocr(input,'CharacterSet','0123456789');
+duoti_skaiciai = atsakymas.Text;    
+figure(1);
+imshow(input);
+text(600, 150, duoti_skaiciai, 'BackgroundColor', [1 1 1]);
